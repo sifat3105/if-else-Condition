4ffc19e963d8e0243f8605ab139fed298be4ac96
MD SIFAT ALI <sifatali520@gmail.com>
2024-03-13 02:34:28 +0600

    Create if-else2.py
    Take values of length and breadth of a rectangle from user and check if it is square or not.

---
+++ if-else2.py
@@ -0,0 +1,6 @@
+length = int(input("Enter length of Rectangle : "))
+breadth = int(input("Enter breadth of Rectangle : "))
+if length == breadth:
+    print("The Rectamgle is square")
+else:
+    print("The Rectamgle is not square")
