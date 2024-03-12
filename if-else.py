4d3824c8dbb3f3ea625f89f6ac83cac153992cdb
MD SIFAT ALI <sifatali520@gmail.com>
2024-03-13 02:13:11 +0600

    Create if-else.py

---
+++ if-else.py
@@ -0,0 +1,9 @@
+locat = input( "whare want to travale: Camilla, Chittogong, Rajshahi : ")
+if locat == "Camilla":
+    print("travle cost of Camilla 500 taka")
+elif locat == "Chittogong":
+    print("travle cost of Chittogong 1000 taka")
+elif locat == "Rajshahi":
+    print("travle cost of Rajshahi 1200 taka")
+else:
+    print("Wrong type or place")
