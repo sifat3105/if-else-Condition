b336ba8b3d6c9b63f8bf3f381112aaf5817e3fd4
MD SIFAT ALI <sifatali520@gmail.com>
2024-03-14 00:45:20 +0600

    Create asking.py

---
+++ asking.py
@@ -0,0 +1,7 @@
+asked = input("You Have Medical Couse (Yes or No) : ")
+if asked == "Yes":
+    print("You are allowed to sit due to a medical cause.")
+elif asked == "no":
+    print("You are not allowed to sit due to a medical cause.")
+else:
+    print("please Answer in 'Yes' or 'No' ")
