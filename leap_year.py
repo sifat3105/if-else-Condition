9acdeb6e4af563ea78d826cf7dae0b40361e0b58
MD SIFAT ALI <sifatali520@gmail.com>
2024-03-14 16:20:28 +0600

    Create leap_year.py

---
+++ leap_year.py
@@ -0,0 +1,13 @@
+year = int(input("enter year : "))
+
+if year%4 == 0:
+    if year%100 == 0:
+        if year%400 == 0:
+            print("Leap Year")
+        else:
+            print("Not Leap Year")
+    else:
+        print("Leap Year")
+else:
+    print("Not Leap Year")
+    
